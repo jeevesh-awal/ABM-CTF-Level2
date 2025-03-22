1327859ebf44824264d1329c4291e393ee5ac421
Jeevesh Awal <32438021+jeevesh-awal@users.noreply.github.com>
2025-03-22 13:40:03 -0400

    Update ch3.js

--- scripts/ch3.js
+++ scripts/ch3.js
@@ -1,5 +1,5 @@
 
 // The flag is encrypted below. Decode it :
-// QkJNe2pzX2NvbW1lbnRzX2FyZV9mdW59
+// QUJNe2pzX2NvbW1lbnRzX2FyZV9mdW59
 
 console.log("Welcome to Challenge 3! Inspect this JavaScript file to find the flag.");
